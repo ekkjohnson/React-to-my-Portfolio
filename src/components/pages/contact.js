77313b67cc4e8f177e2a5330abcc67a8c4ebe32b
Elizabeth <ekkjohnson@gmail.com>
2022-05-24 18:55:06 -0500

    contact info

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,49 +1,49 @@
-// import React, {useRef} from 'react';
-// // import emailjs from '@emailjs/browser';
-// import  Form  from 'react-bootstrap/Form';
-// import  Button  from 'react-bootstrap/Button';
-// import  Container  from 'react-bootstrap/Container';
+import React, {useRef} from 'react';
+import emailjs from '@emailjs/browser';
+import  Form  from 'react-bootstrap/Form';
+import  Button  from 'react-bootstrap/Button';
+import  Container  from 'react-bootstrap/Container';
 
-// export default function Contact() {
-//   const form = useRef();
+export default function Contact() {
+  const form = useRef();
 
-// //   const sendEmail = (e) => {
-// //     e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-// //     emailjs.sendForm('service_swbi2lm', 'template_f5q6z22', form.current, 'K0BqNsVgUP9MWXugB')
-// //       .then((result) => {
-// //           console.log(result.text);
-// //           alert('Thank You, your e-mail has been sent')
-// //       }, (error) => {
-// //           console.log(error.text);
-// //       });
-//   };
-//   return (
-//     <div>
-//       <h1 style={{marginLeft: 20, fontFamily: 'Merriweather serif', fontSize: 45, marginTop: 10}}>Contact</h1>
-//       <hr></hr>
-//       <Container>
+    emailjs.sendForm('service_swbi2lm', 'template_f5q6z22', form.current, 'K0BqNsVgUP9MWXugB')
+      .then((result) => {
+          console.log(result.text);
+          alert('Thank you for you email, it has been sent!')
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+  return (
+    <div>
+      <h1 style={{marginLeft: 20, fontFamily: 'Merriweather serif', fontSize: 45, marginTop: 10}}>Contact</h1>
+      <hr></hr>
+      <Container>
 
-//       <Form ref={form} onSubmit={sendEmail} style={{background: '#cce3de', padding: 10, marginTop: 20}}>
-//       <Form.Group className="mb-3" controlId="formBasicEmail">
-//           <Form.Label>Name</Form.Label>
-//           <Form.Control type="text" placeholder="Please enter your name" name="from_name"/>
-//         </Form.Group>
+      <Form ref={form} onSubmit={sendEmail} style={{background: '#cce3de', padding: 10, marginTop: 20}}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Please enter your name" name="from_name"/>
+        </Form.Group>
 
-//         <Form.Group className="mb-3" controlId="formBasicEmail">
-//           <Form.Label>Email address</Form.Label>
-//           <Form.Control type="email" placeholder="Please enter your email address" name="user_email"/>
-//         </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Please enter your email address" name="user_email"/>
+        </Form.Group>
 
-//         <Form.Group className="mb-3" controlId="formBasicPassword">
-//           <Form.Label>Email Contents</Form.Label>
-//           <Form.Control  as="textarea" aria-label="With textarea" placeholder="Your message here..." name="message"/>
-//         </Form.Group>
-//         <Button style={{background: '#6b9080', borderColor:'#6b9080'}} type="submit" value="Send">
-//           Send
-//         </Button>
-//       </Form>
-//       </Container>
-//       </div>
-//       );
-//       }
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Email Contents</Form.Label>
+          <Form.Control  as="textarea" aria-label="With textarea" placeholder="Your message here..." name="message"/>
+        </Form.Group>
+        <Button style={{background: '#6b9080', borderColor:'#6b9080'}} type="submit" value="Send">
+          Send
+        </Button>
+      </Form>
+      </Container>
+      </div>
+      );
+      }
--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import Download from '../DownloadButton'
+
+import '../../styles.css'
 export default function Resume() {
+
     return (
   
       <div style={{color: '#0a0908', marginLeft: 25}}>
         <h1 style={{fontFamily: 'Merriweather serif', fontSize: 45, marginTop: 10}}>Resume</h1>
         <hr></hr>
+        <h5>Click Arrow to Download Resume <Download/></h5>
+      <div style={{display: 'flex', flexWrap: 'wrap'}}></div>
         <iframe style={{height:620, width:850, marginLeft: 15}}src="https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:7702f513-af53-4bc2-8188-a3cd4ede8924" title="Resume"></iframe>
       </div>
     );
