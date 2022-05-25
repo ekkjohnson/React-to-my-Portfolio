85bf396173c7abd11a4b8101c671b68c15823cbf
Elizabeth <ekkjohnson@gmail.com>
2022-05-24 19:03:44 -0500

    working fixes

--- src/components/navTabs.js
+++ src/components/navTabs.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import Logo from '../assets/logo_transparent_background.png'
+
 
 
 
@@ -10,7 +10,7 @@ function NavTabs({ handlePageChange}) {
   return (
     <Navbar style={{background: '#6b9080'}}>
     <Container>
-    <Navbar.Brand href="#home"><img src={Logo} style={{width:180, heigth:80}} alt="Logo"/></Navbar.Brand>
+   
     <Nav className="me-auto" style={{fontWeight: 'bold'}}>
       <Nav.Link href="#about" onClick={() => handlePageChange('About')}>About</Nav.Link>
       <Nav.Link href="#work" onClick={() => handlePageChange('Work')}>Work</Nav.Link>
